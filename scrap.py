6cf6cd0dc1671b7664e1108f75dc4959be3d9634
AlexBng <98124921+AlexBng@users.noreply.github.com>
2023-01-18 05:00:59 -0500

    Site scraping is done

--- scrap.py
+++ scrap.py
@@ -44,7 +44,7 @@ i=0
 # Iterate through the keys (i.e., the group ids) in the terrorist_groups dictionary
 for key in terrorist_groups:
 
-    if i==2:
+    if i==20:
         break
     i+=1
     # Initialize an empty list to store the incident ids for the current group
@@ -93,21 +93,34 @@ for key in terrorist_groups:
 
 
 # Creation of a dictionary to store every characteristics of an incident (Sources is a list of URLs)
-characteristics = {'Date': None, 'Country': None, 'Region': None, 'Province': None, 'City': None, 'TypeOfAttack': None, 'Successful': None, 
-'TargetType': None, 'EntityName': None, 'SpecificDescription': None, 'TargetNationality': None, 'Hostages': None, 'Ransom': None, 'PropertyDamage': None, 
-'PropertyDamageExtent': None, 'PropertyDamageValue': None, 'WeaponType': None, 'SubWeaponType': None, 'WeaponDetails': None, 'SuicideAttack': None, 
-'PartOfMultipleIncident': None, 'Criterion1': None, 'Criterion2': None, 'Criterion3': None, 'ClaimedResponsibility': None, 
+characteristics = {'Description':None, 'Date': None, 'Country': None, 'Region': None, 'Province': None, 'City': None, 'LocationDetails': None,
+ 'TypeOfAttack1': None, 'TypeOfAttack2': None, 'TypeOfAttack3': None, 'Successful': None, 
+'TargetType1': None, 'TargetType2': None,'TargetType3': None,'EntityName1': None, 'EntityName2': None, 'EntityName3': None,
+'SpecificDescription1': None, 'SpecificDescription2': None,'SpecificDescription3': None, 'TargetNationality1': None, 'TargetNationality2': None,
+'TargetNationality3': None, 'Hostages': None, 'HostagesNumber':None, 'Ransom': None, 'PropertyDamage': None, 
+'PropertyDamageExtent': None, 'PropertyDamageValue': None, 'WeaponType1': None, 'SubWeaponType1': None, 'WeaponType2': None, 'SubWeaponType2': None,
+'WeaponType3': None, 'SubWeaponType3': None, 'WeaponType4': None, 'SubWeaponType4': None,'WeaponDetails': None, 'SuicideAttack': None, 
+'PartOfMultipleIncident': None, 'Criterion1': None, 'Criterion2': None, 'Criterion3': None, 'DoubtTerrorismProper': None, 'GroupName' : None,
 'PerpetratorsNumber': None, 'CapturedPerpetratorsNumber': None, 'CasualitiesNumber': None, 'FatalitiesNumber': None, 'PerpetratorsFatalitiesNumber': None, 
 'InjuredNumber': None, 'PerpetratorsInjuredNumber': None, 'Sources': None}
 
 for key in incidents_characteristics:
 
-    url_incident = "https://www.start.umd.edu/gtd/search/IncidentSummary.aspx?gtdid=199211060007"
+    url_incident = "https://www.start.umd.edu/gtd/search/IncidentSummary.aspx?gtdid=" + key
 
     response = requests.get(url_incident)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Get every informations of the summary-overview
+
+    # The description isn't present everytime
+    description = soup.select_one('#incidentSummary')
+    if(description): 
+        text = description.find('div').find('h1').next_sibling
+        text = text.replace("\n\t\t","")
+        characteristics['Description'] = text.replace("\n\t","")
+
+
     for p in soup.select('.summary-overview p'):
         head = p.find("span", class_="leftHead").text
         span_text = p.find("span", class_="leftLarge").text
@@ -121,19 +134,108 @@ for key in incidents_characteristics:
             characteristics['Province'] = span_text
         elif "City:" in head:
             characteristics['City'] = span_text
+        elif "Location Details:" in head:
+            characteristics['LocationDetails'] = span_text
 
     # Get informations of WHAT
-    characteristics['TypeOfAttack'] = soup.select_one('#popupTactics').find_parent('td').find_next_sibling('td').text
+
+    # A minimum of 1 type of attack and a miximum of 3 is printed on the website
+    attackType = soup.select('#popupTactics')
+    characteristics['TypeOfAttack1'] = attackType[0].find_parent('td').find_next_sibling('td').text
+    if(len(attackType) > 1):
+        characteristics['TypeOfAttack2'] = attackType[1].find_parent('td').find_next_sibling('td').text
+    if(len(attackType) > 2):
+        characteristics['TypeOfAttack3'] = attackType[2].find_parent('td').find_next_sibling('td').text
+
     characteristics['Successful'] = soup.select_one('#popupSuccess').find_parent('td').find_next_sibling('td').text
-    characteristics['TargetType'] = soup.find("th", text=lambda text: "Target Type:" in text).text.replace("Target Type: ","")
-    characteristics['EntityName'] = soup.find("td", text="Name of Entity").find_next_sibling("td").text
-    characteristics['SpecificDescription'] = soup.find("td", text="Specific Description").find_next_sibling("td").text
-    characteristics['TargetNationality'] = soup.find("td", text="Nationality of Target").find_next_sibling("td").text
+
+    targetType = soup.find_all("th", text=lambda text: "Target Type:" in text)
+    entityName = soup.find_all("td", text="Name of Entity")
+    specificDescription = soup.find_all("td", text="Specific Description")
+    targetNationality = soup.find_all("td", text="Nationality of Target")
+    characteristics['TargetType1'] = targetType[0].text.replace("Target Type: ","")
+    characteristics['EntityName1'] = entityName[0].find_next_sibling("td").text
+    characteristics['SpecificDescription1'] = specificDescription[0].find_next_sibling("td").text
+    characteristics['TargetNationality1'] = targetNationality[0].find_next_sibling("td").text
+    if(len(targetType) > 1):
+        characteristics['TargetType2'] = targetType[1].text.replace("Target Type: ","")
+        characteristics['EntityName2'] = entityName[1].find_next_sibling("td").text
+        characteristics['SpecificDescription2'] = specificDescription[1].find_next_sibling("td").text
+        characteristics['TargetNationality2'] = targetNationality[1].find_next_sibling("td").text
+    if(len(targetType) > 2):
+        characteristics['TargetType3'] = targetType[2].text.replace("Target Type: ","")
+        characteristics['EntityName3'] = entityName[2].find_next_sibling("td").text
+        characteristics['SpecificDescription3'] = specificDescription[2].find_next_sibling("td").text
+        characteristics['TargetNationality3'] = targetNationality[2].find_next_sibling("td").text
+
+
     characteristics['Hostages'] = soup.find("td", text="Hostages").find_next_sibling("td").text
+
+    number = soup.find("td", text="Number of Hostages")
+    if(number):
+        characteristics['HostagesNumber'] = number.find_next_sibling("td").text
+    # Ransom fields missing (ex : 197802190002)
     characteristics['Ransom'] = soup.find("td", text="Ransom").find_next_sibling("td").text
     characteristics['PropertyDamage'] = soup.find("td", text="Property Damage").find_next_sibling("td").text
-    characteristics['PropertyDamageExtent'] = soup.find("td", text="Extent of Property Damage").find_next_sibling("td").text
-    characteristics['PropertyDamageValue'] = soup.find("td", text="Value of Property Damage").find_next_sibling("td").text
 
+    extent = soup.find("td", text="Extent of Property Damage")
+    if(extent):
+        characteristics['PropertyDamageExtent'] = extent.find_next_sibling("td").text
+    value = soup.find("td", text="Value of Property Damage")
+    if(value):
+        characteristics['PropertyDamageValue'] = value.find_next_sibling("td").text
 
+    # Get informations of HOW
+    # Between 1 and 4 weapon
+    weapon = soup.find("th", text="Type").find_parent('tr').find_parent('thead').find_next_sibling('tbody').find_all("tr")
+    characteristics['WeaponType1'] = weapon[0].find_all('td')[0].text
+    characteristics['SubWeaponType1'] = weapon[0].find_all('td')[1].text
+    if(len(weapon) > 1  and len(weapon[1].find_all('td')) == 2):
+        characteristics['WeaponType2'] = weapon[1].find_all('td')[0].text
+        characteristics['SubWeaponType2'] = weapon[1].find_all('td')[1].text
+    if(len(weapon) > 2 and len(weapon[2].find_all('td')) == 2):
+        characteristics['WeaponType3'] = weapon[2].find_all('td')[0].text
+        characteristics['SubWeaponType3'] = weapon[2].find_all('td')[1].text
+    if(len(weapon) > 3 and len(weapon[3].find_all('td')) == 2):
+        characteristics['WeaponType4'] = weapon[3].find_all('td')[0].text
+        characteristics['SubWeaponType4'] = weapon[3].find_all('td')[1].text
+
+    details = soup.find("th", text="Weapon Details")
+    if(details):
+        characteristics['WeaponDetails'] = details.find_parent('tr').find_next_sibling('tr').find('td').text
+    characteristics['SuicideAttack'] = soup.find("td", text="Suicide Attack?").find_next_sibling('td').text
+    characteristics['PartOfMultipleIncident'] = soup.find("td", text="Part of Multiple Incident?").find_next_sibling('td').text
+    characteristics['Criterion1'] = soup.select_one('#popupCriterionOne').find_parent('td').find_next_sibling('td').text
+    characteristics['Criterion2'] = soup.select_one('#popupCriterionTwo').find_parent('td').find_next_sibling('td').text
+    characteristics['Criterion3'] = soup.select_one('#popupCriterionThree').find_parent('td').find_next_sibling('td').text
+
+    # Field not present everytime
+    doubt = soup.select_one('#popupCriteriaDTP')
+    if doubt:
+        characteristics['DoubtTerrorismProper'] = doubt.find_parent('td').find_next_sibling('td').text
+
+    # Get informations of WHO
+    # Responsability and sub-name fields missing, multiple groups possible
+    groups = []
+    for tr in soup.find("th", text="Group Name").find_parent('tr').find_parent('thead').find_next_sibling('tbody').find_all("tr"):
+        groups.append(tr.find_all('td')[0].text)
+    characteristics['GroupName'] = groups
+
+    characteristics['PerpetratorsNumber'] = soup.find("td", text="Number of Perpetrators").find_next_sibling('td').text
+    characteristics['CapturedPerpetratorsNumber'] = soup.find("td", text="Number of Captured Perpetrators").find_next_sibling('td').text
+    characteristics['CasualitiesNumber'] = soup.find("td", text="Total Number of Casualties").find_next_sibling('td').text
+    characteristics['FatalitiesNumber'] = soup.find("td", text="Total Number of Fatalities").find_next_sibling('td').text
+    characteristics['PerpetratorsFatalitiesNumber'] = soup.find("td", text="Number of Perpetrator Fatalities").find_next_sibling('td').text
+    characteristics['InjuredNumber'] = soup.find("td", text="Total Number of Injured").find_next_sibling('td').text
+    characteristics['PerpetratorsInjuredNumber'] = soup.find("td", text="Number of Perpetrators Injured").find_next_sibling('td').text
+
+    # Creation of the description if it doesn't exist
+    if(characteristics['Description'] == None):
+        characteristics['Description'] = characteristics['Date'] + ": " + " ".join(characteristics['GroupName']) + " operated an attack in " + characteristics['Country'] + "."
+
+    # Sources
+    sources = []
+    for tr in soup.find('caption', text='Sources').find_next_sibling('tbody').find_all('tr'):
+        sources.append(tr.find('td').text)
+    characteristics['Sources'] = sources
     print(characteristics)
